9bf4e62eb0b875e57fe6d98fd3d0860c5ece0484
tastytim <tastytimgm@gmail.com>
2021-11-15 16:26:53 +0100

    commit 2

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="cardcontainer">
     <div class="container">
-      <div class="row row-cols-5 g-4">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-4">
           <div class="col" v-for="(item, index) in cardsList" :key="index">
         <Card
           :poster="item.poster"
